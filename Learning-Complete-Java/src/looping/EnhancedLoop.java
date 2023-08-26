9e71c4bba2704887f6c0ad5c7f3211188a874eb3
akashydv311 <akashydv311@gmail.com>
2023-08-27 00:51:10 +0530

    Looping completed

---
+++ Learning-Complete-Java/src/looping/EnhancedLoop.java
@@ -0,0 +1,22 @@
+package looping;
+
+public class EnhancedLoop {
+
+    public void example() {
+
+        int nums[][] = new int[3][4];
+        // by defualt all values ore 0
+
+        for (int arr[] : nums) {
+            for (int value : arr) {
+                System.out.print(value + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        EnhancedLoop el = new EnhancedLoop();
+        el.example();
+    }
+}
