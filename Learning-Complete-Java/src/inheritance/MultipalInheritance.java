b1b2dfda41396bbe51e15ad9739606aa08afc762
akashydv311 <akashydv311@gmail.com>
2023-08-27 20:02:57 +0530

    Inheritance updated

--- Learning-Complete-Java/src/inheritance/MultipalInheritance.java
+++ Learning-Complete-Java/src/inheritance/MultipalInheritance.java
@@ -20,10 +20,34 @@ class B {
  * calles??
  * it's an ambiguity for java that's the reasion why java dosn't support
  * multipal inheritanc
+ * we can do multipal inheritance with the help of interfaces
  */
+
+interface Swimmer {
+    void swim();
+}
+
+interface Flyer {
+    void fly();
+}
+
+class Duck implements Swimmer, Flyer {
+    @Override
+    public void swim() {
+        System.out.println("Duck is swimming.");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println("Duck is flying.");
+    }
+}
 
 public class MultipalInheritance {
     public static void main(String[] args) {
-
+        // indirect way of doing multipal inheritance
+        Duck duck = new Duck();
+        duck.fly();
+        duck.swim();
     }
 }
