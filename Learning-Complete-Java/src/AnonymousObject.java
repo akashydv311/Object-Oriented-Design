67455c673555bb066570666a6c06764732aaa2d2
akashydv311 <akashydv311@gmail.com>
2023-08-27 18:42:25 +0530

    Anonymous Object added

---
+++ Learning-Complete-Java/src/AnonymousObject.java
@@ -0,0 +1,15 @@
+
+class Demo {
+
+    Demo() {
+        System.out.println("Obj creted!");
+    }
+}
+
+public class AnonymousObject {
+
+    public static void main(String[] args) {
+
+        new Demo(); // Anonymous object
+    }
+}
