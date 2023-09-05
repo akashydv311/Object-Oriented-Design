fdaa1d1d0c36066d363272c1d58214d60ef7bc10
akashydv311 <akashydv311@gmail.com>
2023-09-05 19:16:55 +0530

    enum example updated

--- Learning-Complete-Java/src/enumexample/Test.java
+++ Learning-Complete-Java/src/enumexample/Test.java
@@ -9,5 +9,13 @@ public class Test {
         // System.out.println("Hello, World!");
         Status s1 = Status.Studying;
         System.out.println(s1);
+        System.out.println(s1.ordinal());
+
+        Status[] all = Status.values();
+        // System.out.println(all[0]);
+
+        for (Status allstatus : all) {
+            System.out.println(allstatus);
+        }
     }
 }
